0e5a26f009d652275b30c7d1b768ca4bcfb77e6e
arabam13 <araba.mohamed@gmail.com>
2022-03-26 02:24:39 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import '../css/app.css';
+
+let $ = require('jquery')
+
+$(function(){
+
+    $(".navbar a, footer a").on("click", function(event){
+    
+        event.preventDefault();
+        var hash = this.hash;
+        
+        $('body,html').animate({
+                                scrollTop: $(hash).offset().top
+                                } , 
+                                900 , 
+                                function(){
+                                    window.location.hash = hash;
+                                })
+        
+    });
+
+})
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+// import $ from 'jquery';
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
